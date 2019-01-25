2cbc530a8d1f0587e8bc1b7f13da24528518a9d3
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-25 01:03:38 -0800

    commit

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class TransitionComp extends Component{
+    state = {
+        show: false
+    }
+    showDiv = () => {
+        this.setState({
+            show:!this.state.show ? true:false
+        })
+    }
 
     render(){
         return(
             <div>
-                Transition
-            </div>
+                {this.state.show ? <div style={{
+                    background:'red',
+                    height:'100px'
+                }}>
 
+                </div>:
+            null }
+            <div className="showDiv"
+            onClick={this.showDiv}>
+                Show or Hide
+            </div>
+            </div>
         )
     }
 }
