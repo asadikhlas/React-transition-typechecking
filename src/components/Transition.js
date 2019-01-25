141942e2dec835cb83381199a08f458555cc9a0e
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-25 01:30:56 -0800

    commit

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
-import Transition from 'react-transition-group/Transition';
-import '../css/App.css';
+import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
+import "../css/App.css";
 
-class TransitionComp extends Component{
-    state = {
-        show: true
-    }
-    showDiv = () => {
-        this.setState({
-            show:!this.state.show ? true:false
-        })
-    }
+class TransitionComp extends Component {
+  state = {
+    show: true
+  };
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show ? true : false
+    });
+  };
 
-    render(){
-        return(
-            <div>
-                <Transition 
-                in={this.state.show}
-                timeout={2000}
-                mountOnEnter
-                unmountOnExit
-                >
-                {state =>
-                <div style={{
-                    background:'red',
-                    height:'100px',
-                    transition:'all 2s ease',
-                    opacity: state  === 'exited' ||state === 'exiting' ? 0 : 1
-                    
-                }}>
-                        {state}
-                </div>
-
-                }
-                </Transition>
-            <div className="showDiv"
-            onClick={this.showDiv}>
-                Show or Hide
+  render() {
+    return (
+      <div>
+        <Transition
+          in={this.state.show}
+          timeout={2000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <div
+              style={{
+                background: "red",
+                height: "100px",
+                transition: "all 2s ease",
+                opacity: state === "exited" || state === "exiting" ? 0 : 1
+              }}
+            >
+              {state}
             </div>
-            </div>
-        )
-    }
+          )}
+        </Transition>
+        <div className="showDiv" onClick={this.showDiv}>
+          Show or Hide
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default TransitionComp;
